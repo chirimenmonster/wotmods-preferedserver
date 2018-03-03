e9601189dd69dd5bfe26f6bef334a3a51b781ce2
chirimenmonster <chirimenmonster@users.noreply.github.com>
2018-03-03 21:47:31 +0900

    update packaging script

--- package.py
+++ package.py
@@ -8,15 +8,17 @@ from string import Template
 
 BUILD_DIR = 'build'
 CONFIG = 'config.ini'
-FILELIST = 'files.json'
+FILE_LIST = 'files.json'
+RELEASE_LIST = 'release.json'
 
 def get_config():
     inifile = ConfigParser.SafeConfigParser()
     inifile.read(CONFIG)
 
     parameters = dict(inifile.items('mod'))
-    for k, v in inifile.items('wot'):
-        parameters['wot_' + k] = v
+    for section in inifile.sections():
+        for k, v in inifile.items(section):
+            parameters[section + '_' + k] = v
     return parameters
 
 
@@ -27,12 +29,12 @@ def compile_python(src, dst, virtualdir):
         pass
     py_compile.compile(file=src, cfile=dst, dfile=os.path.join(virtualdir, os.path.basename(src)), doraise=True)
 
-def apply_template(src, dstdir, parameters):
+def apply_template(src, dst, parameters):
     try:
-        os.makedirs(os.path.dirname(os.path.join(dstdir, src)))
+        os.makedirs(os.path.dirname(dst))
     except:
         pass
-    with open(src, 'r') as in_file, open(os.path.join(dstdir, src), 'w') as out_file:
+    with open(src, 'r') as in_file, open(dst, 'w') as out_file:
         out_file.write(Template(in_file.read()).substitute(parameters))
 
 def split(path):
@@ -43,56 +45,82 @@ def split(path):
     result.append(path)
     return result
 
-def read_filelist(parameters=None):
+def process_filelist(parameters, desc):
     paths = []
-    with open(FILELIST, 'r') as f:
-        desc = json.load(f)
-        for target in desc:
-            if target['method'] == 'apply+python':
-                for src in target['files']:
-                    root, ext = os.path.splitext(src)
-                    dst = root + '.pyc'
-                    apply_template(src, BUILD_DIR, parameters)
-                    compile_python(os.path.join(BUILD_DIR, src), os.path.join(BUILD_DIR, dst), target['reldir'])
-                    info = [ dst, os.path.join(target['root'], target['reldir'], os.path.basename(dst)) ]
-                    paths.append(info)
-            elif target['method'] == 'python':
-                for src in target['files']:
-                    root, ext = os.path.splitext(src)
-                    dst = root + '.pyc'
-                    compile_python(src, os.path.join(BUILD_DIR, dst), target['reldir'])
-                    info = [ dst, os.path.join(target['root'], target['reldir'], os.path.basename(dst)) ]
-                    paths.append(info)
-            elif target['method'] == 'apply':
-                for src in target['files']:
-                    apply_template(src, BUILD_DIR, parameters)
-                    paths.append((src, os.path.join(target['root'], target['reldir'], src)))
-            elif target['method'] == 'plain':
-                for src in target['files']:
-                    shutil.copy(src, BUILD_DIR)      
-                    paths.append((src, os.path.join(target['root'], target['reldir'], src)))
+    for target in desc:
+        if target['method'] == 'apply+python':
+            for src in target['files']:
+                root, ext = os.path.splitext(src)
+                dst = root + '.pyc'
+                prepared = os.path.join(BUILD_DIR, src)
+                cooked = os.path.join(BUILD_DIR, dst)
+                release = os.path.join(target['root'], target['reldir'], os.path.basename(dst))
+                apply_template(src, prepared, parameters)
+                compile_python(prepared, cooked, target['reldir'])
+                paths.append([ cooked, release ])
+        elif target['method'] == 'python':
+            for src in target['files']:
+                root, ext = os.path.splitext(src)
+                dst = root + '.pyc'
+                cooked = os.path.join(BUILD_DIR, dst)
+                release = os.path.join(target['root'], target['reldir'], os.path.basename(dst))
+                compile_python(src, cooked, target['reldir'])
+                paths.append([ cooked, release])
+        elif target['method'] == 'apply':
+            for src in target['files']:
+                cooked = os.path.join(BUILD_DIR, src)
+                release = os.path.join(target['root'], target['reldir'], os.path.basename(src))
+                apply_template(src, cooked, parameters)
+                paths.append([ cooked, release ])
+        elif target['method'] == 'plain':
+            for src in target['files']:
+                cooked = src
+                release = os.path.join(target['root'], target['reldir'], os.path.basename(src))
+                paths.append([ cooked, release ])
     return paths
 
+
+def create_zipfile(package, paths, compression=zipfile.ZIP_DEFLATED):
+    donelist = []
+    with zipfile.ZipFile(package, 'w', compression=compression) as file:
+        for source, target in paths:
+            for dir in split(target)[0:-1]:
+                if dir not in donelist:
+                    file.write('.', dir, compression)
+                    donelist.append(dir)
+            file.write(source, target, compression)
+            donelist.append(target)
+
+
+def create_wotmod(jsonfile, params):
+    with open(jsonfile, 'r') as f:
+        desc = json.loads(Template(f.read()).substitute(params))
+    paths = process_filelist(params, desc['files'])
+    package = os.path.join(BUILD_DIR, params['package'])
+    create_zipfile(package, paths, zipfile.ZIP_STORED)
+
+
+def create_release(jsonfile, params):
+    with open(jsonfile, 'r') as f:
+        desc = json.loads(Template(f.read()).substitute(params))
+    paths = process_filelist(params, desc['files'])
+    package = os.path.join(BUILD_DIR, desc['package'])
+    create_zipfile(package, paths, zipfile.ZIP_DEFLATED)
+
+
 def main():
-    parameters = get_config()
+    params = get_config()
     try:
         shutil.rmtree(BUILD_DIR)
     except:
         pass
     os.makedirs(BUILD_DIR)
 
-    paths = read_filelist(parameters=parameters)
+    create_wotmod(FILE_LIST, params)
 
-    package_path = os.path.join(BUILD_DIR, parameters['package'])
-    donelist = []
-    with zipfile.ZipFile(package_path, 'w', compression=zipfile.ZIP_STORED) as package_file:
-        for source, target in paths:
-            for dir in split(target)[0:-1]:
-                if dir not in donelist:
-                    package_file.write('.', dir, zipfile.ZIP_STORED)
-                    donelist.append(dir)
-            package_file.write(os.path.join(BUILD_DIR, source), target, zipfile.ZIP_STORED)
-            donelist.append(target)
+    if os.path.exists(RELEASE_LIST):
+        create_release(RELEASE_LIST, params)
+
 
 if __name__ == "__main__":
     main()
